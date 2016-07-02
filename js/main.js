55ee9be5bdee7295903a33b0535d8de8877307fb
Benjamin Lannon <lannonbr@clarkson.edu>
2016-07-02 16:29:47 -0400

    Added sidenav and footer

--- js/main.js
+++ js/main.js
@@ -5,3 +5,20 @@ $(window).scroll(function() {
 		$('.header-fixed').removeClass('header-shadow');
 	}
 });
+
+$(function() {
+	$(".btn-navdrawer").click(function() {
+
+		$(".navdrawer-open-shadow").css("display", "block").animate({"opacity": 1.0}, 400);
+		$(".navdrawer").css("display", "block").animate({"left": "0px"}, 400);
+
+		// $(".navdrawer-open-shadow").show();
+		// $(".navdrawer").show();
+	});
+
+	$(".navdrawer-open-shadow").click(function() {
+		$(".navdrawer-open-shadow").animate({"opacity": 0.0}, 300).css("display", "none");
+		$(".navdrawer").animate({"left": "-320px"}, 300);
+
+	});
+});
